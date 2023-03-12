ec765f69afc9128789af3264f897cf0ebb21f567
d-ivashchuk <dimaiv@me.com>
2023-03-12 13:05:19 +0100

    feat: add ability to converse

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { Telegraf } from "telegraf";
+import { Telegraf, session, Context } from "telegraf";
 import { OpenAIApi, Configuration } from "openai";
 import { z } from "zod";
 import { config } from "../config";
@@ -21,6 +21,18 @@ const COMPLETION_PARAMS = {
   top_p: 1
 };
 
+interface SessionData {
+  mode?: string;
+  chatHistory?: {
+    role: "system" | "user" | "assistant";
+    content: string;
+  }[];
+}
+
+interface MyContext extends Context {
+  session?: SessionData;
+}
+
 const selectedMode = "ASSISTANT";
 
 const env = envSchema.parse(process.env);
@@ -30,7 +42,7 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const bot = new Telegraf(env.TELEGRAM_TOKEN);
+const bot = new Telegraf<MyContext>(env.TELEGRAM_TOKEN);
 
 const app = express();
 
@@ -56,23 +68,63 @@ bot.start((ctx) => {
     "Welcome to the Ed GPT! I'm a bot that can create textual content based on your input. To get started, send me a message with the text you want me to generate content for."
   );
 });
+
+bot.use(session());
+
+bot.command("clear", (ctx) => {
+  if (ctx.session) {
+    ctx.session.chatHistory = undefined;
+
+    const randomStartFromScratchMessages = [
+      "Let's start from the scratch! How can I help?",
+      "Let's start over! What might I assist you with?",
+      "Let's start again! What's on your mind?"
+    ];
+    const pickRandomMessage =
+      randomStartFromScratchMessages[
+        Math.floor(Math.random() * randomStartFromScratchMessages.length)
+      ];
+    ctx.reply(pickRandomMessage);
+  }
+});
+
 bot.on("text", async (ctx) => {
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
+  if (!ctx.session) {
+    ctx.session = {
+      mode: selectedMode
+    };
+  }
+
+  ctx.sendChatAction("typing");
+
+  if (!ctx.session.chatHistory) {
+    ctx.session.chatHistory = [
       {
         role: "system",
         content: modes.find((m) => m.code === selectedMode)
           ?.promptStart as string
       },
       { role: "user", content: ctx.message.text }
-    ],
+    ];
+  } else {
+    ctx.session.chatHistory.push({ role: "user", content: ctx.message.text });
+  }
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: ctx.session.chatHistory,
     ...COMPLETION_PARAMS
   });
 
-  ctx.sendChatAction("typing");
+  if (completion.data.choices[0].message) {
+    ctx.session.chatHistory?.push(completion.data.choices[0].message);
+  }
 
-  ctx.reply(completion.data.choices[0].message?.content as string);
+  ctx.reply(completion.data.choices[0].message?.content as string, {
+    parse_mode: modes.find((m) => m.code === selectedMode)?.parseMode as
+      | "HTML"
+      | "MarkdownV2"
+  });
 });
 
 bot.launch();
