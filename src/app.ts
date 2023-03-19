6808e2a1397c832ce8e2556249137f2ca6a898ec
d-ivashchuk <dimaiv@me.com>
2023-03-19 08:45:51 +0100

    feat: refactor code

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ import { FileFlavor, hydrateFiles } from "@grammyjs/files";
 import { Client } from "pg";
 
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
 dotenv.config();
 
 const COMPLETION_PARAMS = {
@@ -29,20 +30,36 @@ const COMPLETION_PARAMS = {
   top_p: 1
 };
 
-interface SessionData {
+type ChatHistoryItem = {
+  role: "system" | "user" | "assistant";
+  content: string;
+};
+
+type SessionData = {
   mode?: string;
   token?: string;
   messagesCount: number;
-  chatHistory?: {
-    role: "system" | "user" | "assistant";
-    content: string;
-  }[];
+  chatHistory?: ChatHistoryItem[];
   settings?: {
     [key: string]: boolean;
   };
-}
+};
 
 type MyContext = FileFlavor<Context> & SessionFlavor<SessionData>;
+
+const envSchema = z.object({
+  TELEGRAM_TOKEN: z.string().nonempty(),
+  OPEN_AI_PLATFORM_TOKEN: z.string().nonempty(),
+  NO_OAI_TOKEN_MESSAGE_LIMIT: z.string().nonempty(),
+  PG_HOST: z.string().nonempty(),
+  PG_USER: z.string().nonempty(),
+  PG_PASSWORD: z.string().nonempty(),
+  PG_PORT: z.string().nonempty(),
+  PG_DB: z.string().nonempty(),
+  WEBHOOK_DOMAIN: z.string().nonempty(),
+  WEBHOOK_PORT: z.string().nonempty()
+});
+const env = envSchema.parse(process.env);
 
 const selectedMode = "ASSISTANT";
 
@@ -74,20 +91,6 @@ app.use((_req: Request, res: Response, next: NextFunction) => {
 });
 
 async function bootstrap() {
-  const envSchema = z.object({
-    TELEGRAM_TOKEN: z.string().nonempty(),
-    OPEN_AI_PLATFORM_TOKEN: z.string().nonempty(),
-    NO_OAI_TOKEN_MESSAGE_LIMIT: z.string().nonempty(),
-    PG_HOST: z.string().nonempty(),
-    PG_USER: z.string().nonempty(),
-    PG_PASSWORD: z.string().nonempty(),
-    PG_PORT: z.string().nonempty(),
-    PG_DB: z.string().nonempty(),
-    WEBHOOK_DOMAIN: z.string().nonempty(),
-    WEBHOOK_PORT: z.string().nonempty()
-  });
-  const env = envSchema.parse(process.env);
-
   const client = new Client({
     user: env.PG_USER,
     host: env.PG_HOST,
@@ -159,173 +162,156 @@ async function bootstrap() {
     }
   });
 
-  bot.on("message:text", async (ctx) => {
-    if (!ctx.session) {
-      ctx.session = {
-        mode: selectedMode,
-        messagesCount: 0
-      };
-    }
+  bot.on("message:text", handleMessage);
 
-    if (ctx.session.messagesCount >= Number(env.NO_OAI_TOKEN_MESSAGE_LIMIT)) {
-      if (!ctx.session.token) {
-        ctx.reply(
-          `You have reached the limit of ${env.NO_OAI_TOKEN_MESSAGE_LIMIT} messages without an OpenAI API token. You can easily get a token for free at https://platform.openai.com/account/api-keys`
-        );
-        return;
-      }
-    }
-
-    try {
-      const configuration = new Configuration({
-        apiKey: ctx.session.token ?? env.OPEN_AI_PLATFORM_TOKEN
-      });
-
-      const openai = new OpenAIApi(configuration);
-
-      await ctx.replyWithChatAction("typing");
-
-      const prompt = `${ctx.message?.text}. ${
-        ctx.session?.settings?.skipProse ? "Skip prose" : ""
-      }`;
-
-      if (!ctx.session.chatHistory) {
-        ctx.session.chatHistory = [
-          {
-            role: "system",
-            content: modes.find((m) => m.code === selectedMode)
-              ?.promptStart as string
-          },
-          { role: "user", content: prompt }
-        ];
-      } else {
-        ctx.session.chatHistory.push({ role: "user", content: prompt });
-      }
-
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: ctx.session.chatHistory,
-        ...COMPLETION_PARAMS
-      });
-
-      ctx.session.messagesCount = ctx.session.messagesCount + 1;
-
-      if (completion.data.choices[0].message) {
-        ctx.session.chatHistory?.push(completion.data.choices[0].message);
-      }
-
-      ctx.reply(completion.data.choices[0].message?.content as string, {
-        parse_mode: modes.find((m) => m.code === selectedMode)?.parseMode as
-          | "HTML"
-          | "MarkdownV2"
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
-  bot.on("message:voice", async (ctx) => {
-    ctx.reply("🔊 Processing your voice message...");
-    const file = await ctx.getFile(); // valid for at least 1 hour
-    const downloadedFilePath = await file.download();
-
-    try {
-      ffmpeg(downloadedFilePath)
-        .toFormat("mp3")
-
-        .on("start", function (commandLine) {
-          console.log("Spawned Ffmpeg with command: " + commandLine);
-        })
-        .on("error", function (err) {
-          console.log(
-            "An error occurred while converting with mmpeg: " + err.message
-          );
-        })
-        .on("end", async function () {
-          console.log("Processing finished !");
-
-          const configuration = new Configuration({
-            apiKey: ctx?.session?.token ?? env.OPEN_AI_PLATFORM_TOKEN
-          });
-
-          const openai = new OpenAIApi(configuration);
-
-          const transcript = await openai.createTranscription(
-            fs.createReadStream(
-              path.join(os.tmpdir(), `${ctx.message.voice.file_id}.mp3`)
-            ) as any,
-            "whisper-1"
-          );
-
-          ctx.reply(`You asked: \n${transcript.data.text}`);
-
-          if (!ctx.session) {
-            ctx.session = {
-              mode: selectedMode,
-              messagesCount: 0
-            };
-          }
-
-          if (
-            ctx.session.messagesCount >= Number(env.NO_OAI_TOKEN_MESSAGE_LIMIT)
-          ) {
-            if (!ctx.session.token) {
-              ctx.reply(
-                `You have reached the limit of ${env.NO_OAI_TOKEN_MESSAGE_LIMIT} messages without an OpenAI API token. You can easily get a token for free at https://platform.openai.com/account/api-keys`
-              );
-              return;
-            }
-          }
-
-          try {
-            await ctx.replyWithChatAction("typing");
-
-            if (!ctx.session.chatHistory) {
-              ctx.session.chatHistory = [
-                {
-                  role: "system",
-                  content: modes.find((m) => m.code === selectedMode)
-                    ?.promptStart as string
-                },
-                { role: "user", content: transcript.data.text }
-              ];
-            } else {
-              ctx.session.chatHistory.push({
-                role: "user",
-                content: transcript.data.text
-              });
-            }
-
-            const completion = await openai.createChatCompletion({
-              model: "gpt-3.5-turbo",
-              messages: ctx.session.chatHistory,
-              ...COMPLETION_PARAMS
-            });
-
-            ctx.session.messagesCount = ctx.session.messagesCount + 1;
-
-            if (completion.data.choices[0].message) {
-              ctx.session.chatHistory?.push(completion.data.choices[0].message);
-            }
-
-            ctx.reply(completion.data.choices[0].message?.content as string, {
-              parse_mode: modes.find((m) => m.code === selectedMode)
-                ?.parseMode as "HTML" | "MarkdownV2"
-            });
-          } catch (error) {
-            console.log(error);
-          }
-        })
-        .output(path.join(os.tmpdir(), `${ctx.message.voice.file_id}.mp3`))
-        .run();
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  bot.on("message:voice", handleVoiceMessage);
 
   bot.start();
 }
 
 bootstrap();
+
+const shouldWarnAboutToken = (ctx: MyContext) =>
+  ctx.session &&
+  ctx.session.messagesCount >= Number(env.NO_OAI_TOKEN_MESSAGE_LIMIT) &&
+  !ctx.session.token;
+
+const warnAboutTokenLimit = (ctx: MyContext) =>
+  ctx.reply(
+    `You have reached the limit of ${env.NO_OAI_TOKEN_MESSAGE_LIMIT} messages without an OpenAI API token. You can easily get a token for free at https://platform.openai.com/account/api-keys`
+  );
+
+const getChatHistory = (ctx: MyContext, content: string): ChatHistoryItem[] => {
+  if (!ctx.session.chatHistory) {
+    ctx.session.chatHistory = [
+      {
+        role: "system",
+        content: modes.find((m) => m.code === selectedMode)
+          ?.promptStart as string
+      },
+      { role: "user", content }
+    ];
+    return ctx.session.chatHistory;
+  } else {
+    ctx.session.chatHistory.push({
+      role: "user",
+      content
+    });
+    return ctx.session.chatHistory;
+  }
+};
+
+const getChatCompletion = async (ctx: MyContext, prompt: string) => {
+  const configuration = new Configuration({
+    apiKey: ctx?.session?.token ?? env.OPEN_AI_PLATFORM_TOKEN
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  await ctx.replyWithChatAction("typing");
+
+  const chatHistory = getChatHistory(ctx, prompt);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: chatHistory,
+    ...COMPLETION_PARAMS
+  });
+
+  ctx.session.messagesCount = ctx.session.messagesCount + 1;
+
+  if (completion.data.choices[0].message) {
+    ctx.session.chatHistory?.push(completion.data.choices[0].message);
+  }
+
+  return completion.data.choices[0].message;
+};
+
+const getPrompt = (ctx: MyContext) => {
+  const prompt = `${ctx.message?.text}. ${
+    ctx.session?.settings?.skipProse ? "Skip prose." : ""
+  }`;
+
+  return prompt;
+};
+
+const replyWithChatCompletion = async (ctx: MyContext, completion: string) => {
+  await ctx.replyWithChatAction("typing");
+  await ctx.reply(completion, {
+    parse_mode: modes.find((m) => m.code === selectedMode)?.parseMode as
+      | "HTML"
+      | "MarkdownV2"
+  });
+};
+
+const handleMessage = async (ctx: MyContext) => {
+  const prompt = getPrompt(ctx);
+
+  if (shouldWarnAboutToken(ctx)) {
+    warnAboutTokenLimit(ctx);
+    return;
+  }
+
+  try {
+    const completion = await getChatCompletion(ctx, prompt);
+    if (!completion) return ctx.reply("Error getting completion from ChatGPT");
+    replyWithChatCompletion(ctx, completion.content);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleVoiceMessage = async (ctx: MyContext) => {
+  ctx.reply("🔊 Processing your voice message...");
+
+  if (shouldWarnAboutToken(ctx)) {
+    warnAboutTokenLimit(ctx);
+    return;
+  }
+
+  const file = await ctx.getFile(); // valid for at least 1 hour
+  const downloadedFilePath = await file.download();
+
+  try {
+    ffmpeg(downloadedFilePath)
+      .toFormat("mp3")
+      .on("error", function (err) {
+        console.log(
+          "An error occurred while converting with mmpeg: " + err.message
+        );
+      })
+      .on("end", async function () {
+        console.log("Processing finished !");
+
+        const configuration = new Configuration({
+          apiKey: ctx?.session?.token ?? env.OPEN_AI_PLATFORM_TOKEN
+        });
+
+        const openai = new OpenAIApi(configuration);
+
+        const transcript = await openai.createTranscription(
+          fs.createReadStream(
+            path.join(os.tmpdir(), `${ctx.message?.voice?.file_id}.mp3`)
+          ) as any,
+          "whisper-1"
+        );
+
+        try {
+          const completion = await getChatCompletion(ctx, transcript.data.text);
+
+          if (!completion)
+            return ctx.reply("Error getting completion from ChatGPT");
+
+          replyWithChatCompletion(ctx, completion?.content);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+      .output(path.join(os.tmpdir(), `${ctx.message?.voice?.file_id}.mp3`))
+      .run();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export { app };
